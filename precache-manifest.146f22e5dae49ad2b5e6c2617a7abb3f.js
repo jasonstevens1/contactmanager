cb5c1b9aa9811edbde907b339551aa3d0b2348d9
Jason Stevens <jason.stevens@shaw.ca>
2018-10-24 11:28:41 -0600

    Updates

---
+++ precache-manifest.146f22e5dae49ad2b5e6c2617a7abb3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "94a8ac1897f30094550a",
+    "url": "/contactmanager/static/js/main.94a8ac18.chunk.js"
+  },
+  {
+    "revision": "527095332f4068a5852b",
+    "url": "/contactmanager/static/js/1.52709533.chunk.js"
+  },
+  {
+    "revision": "94a8ac1897f30094550a",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "527095332f4068a5852b",
+    "url": "/contactmanager/static/css/1.b32f638a.chunk.css"
+  },
+  {
+    "revision": "9ac8d3ecafd3959913144fc849093fbe",
+    "url": "/contactmanager/index.html"
+  }
+];

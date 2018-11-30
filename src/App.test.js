fddcd6d86d7fe4428e8597da0e10e2a59b2370d3
Jason Stevens <jason.stevens@shaw.ca>
2018-11-30 15:53:54 -0700

    Progress update

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+/* it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+}); */
+
+const add = (a, b) => a + b;
+
+test("addTest", () => {
+  expect(add(1, 2)).toBe(3);
 });
